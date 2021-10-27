0e22d2dd0b9e012a48da753bc98db4ac5ef5c5a8
Vitor Campos <vitorcampos742@gmail.com>
2021-10-27 18:04:48 -0300

    final requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let bod = document.querySelector('body')
-let res = Math.ceil(Math.random() * 6)
+let sec = document.querySelector('#sec')
 
 function text(p1) {
     let texto = document.querySelector('#rgb-color')
-    texto.innerHTML = p1
+    texto.innerHTML = 'Qual cor tem ' + p1
 }
 
 function confere(e) {
-    let ans = document.querySelector('#answer')
+    let ans = document.querySelector('#correto')
     if (e.target == ans) {
         p.innerText = 'Acertou!'
     } else {
@@ -15,22 +15,39 @@ function confere(e) {
     }
 }
 
-for (i = 1; i <= 6; i += 1) {
-    let div = document.createElement('div')
-    div.className = 'ball'
-    let cor1 = parseInt(Math.random() * 256)
-    let cor2 = parseInt(Math.random() * 256)
-    let cor3 = parseInt(Math.random() * 256)
-    let a = 'rgb(' + cor1 + ', ' + cor2 + ', ' + cor3 + ')'
-    console.log(a)
-    div.style.background = a
-    div.addEventListener('click', confere)
-    if (i == res) {
-        div.id = 'answer'
-        text(a)
+function iniciar() {
+    let res = Math.ceil(Math.random() * 6)
+    sec.innerHTML = ''
+    for (i = 1; i <= 6; i += 1) {
+        let div = document.createElement('div')
+        div.className = 'ball'
+        let cor1 = parseInt(Math.random() * 256)
+        let cor2 = parseInt(Math.random() * 256)
+        let cor3 = parseInt(Math.random() * 256)
+        let a = 'rgb(' + cor1 + ', ' + cor2 + ', ' + cor3 + ')'
+        div.style.background = a
+        div.addEventListener('click', confere)
+        if (i == res) {
+            div.id = 'correto'
+            text(a)
+        }
+        sec.appendChild(div)
+        p.innerText = 'Escolha uma cor'
     }
-    bod.appendChild(div)
 }
+
+
+
 let p = document.createElement('p')
+p.id = 'answer'
 p.innerText = 'Escolha uma cor'
+
+iniciar()
+
 bod.appendChild(p)
+
+let btn = document.createElement('button')
+btn.id = 'reset-game'
+btn.addEventListener('click', iniciar)
+btn.innerText = 'Reiniciar'
+bod.appendChild(btn)
